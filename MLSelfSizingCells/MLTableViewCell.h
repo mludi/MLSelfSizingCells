6b06218763da58068acef23b18d4c0293d055215
Matthias Ludwig <matt.ludwig@me.com>
2015-07-09 12:34:26 +0200

    added MLTableViewCell

---
+++ MLSelfSizingCells/MLTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MLTableViewCell.h
+//  MLSelfSizingCells
+//
+//  Created by Matthias Ludwig on 09.07.15.
+//  Copyright © 2015 Matthias Ludwig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLTableViewCell : UITableViewCell
+
+@end
---
+++ MLSelfSizingCells/MLTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MLTableViewCell.m
+//  MLSelfSizingCells
+//
+//  Created by Matthias Ludwig on 09.07.15.
+//  Copyright © 2015 Matthias Ludwig. All rights reserved.
+//
+
+#import "MLTableViewCell.h"
+
+@implementation MLTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
