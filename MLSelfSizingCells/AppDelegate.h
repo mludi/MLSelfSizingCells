e0d34f9afb06098ce4e910d86a4d1c9d4a52ba05
Matthias Ludwig <matt.ludwig@me.com>
2015-07-09 11:48:23 +0200

    Initial Commit

---
+++ MLSelfSizingCells/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MLSelfSizingCells
+//
+//  Created by Matthias Ludwig on 09.07.15.
+//  Copyright © 2015 Matthias Ludwig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MLSelfSizingCells/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MLSelfSizingCells
+//
+//  Created by Matthias Ludwig on 09.07.15.
+//  Copyright © 2015 Matthias Ludwig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
